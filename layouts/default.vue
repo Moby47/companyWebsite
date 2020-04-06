97a9c1e69dc3519c95c9f5f9089d5b33fab68d63
henry <henryonyemaobi@gmail.com>
2020-04-06 23:01:34 +0100

    switched js files from head to before body ends

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,64 +1,6 @@
 <template>
   
-    <!--
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
--->
+   
     <div>
     
         <!-- site header -->
@@ -182,7 +124,13 @@ export default {
   head () {
     return {
       script: [
-      //  {src: '/js/particles.min.js'}
+      { src: 'js/jquery-3.3.1.min.js' },
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' },
+      { src: 'js/particles.min.js' },
+      { src: 'js/script.js' },
+      { src: 'js/lightbox-plus-jquery.min.js' },
+      { src: 'js/custom.js' },
+      { src: 'js/owl.carousel.js' },
       ]
     }
   },
