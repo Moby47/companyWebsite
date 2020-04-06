cebdd5f399ccd38e92360572a8af580e67718a55
henry <henryonyemaobi@gmail.com>
2020-04-06 22:23:41 +0100

    added header and footer to layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,46 +2,7 @@
   
 
 <div>
-  <!-- site header -->
-<header id="site-header" class="fixed-top">
-    <nav class="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="index.html">
-            <span class="fa fa-shield"></span> Captivate
-        </a>
-        <button class="navbar-toggler bg-gradient" type="button" data-toggle="collapse"
-            data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav m-auto">
-                <li class="nav-item active">
-         <nuxt-link  class="nav-link" to="/">Home <span class="sr-only">(current)</span></nuxt-link >
-                </li>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="about">About</nuxt-link>
-                </li>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="services">Services</nuxt-link>
-                </li>
-                <li class="nav-item">
-                  <nuxt-link class="nav-link" to="project">portfolio</nuxt-link>
-              </li>
-                <li class="nav-item">
-                    <nuxt-link class="nav-link" to="contact">Contact</nuxt-link>
-                </li>
-            </ul>
-          <!--  <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="#demo" class="btn btn-primary btn-style">View Demo</a>
-                </li>
-            </ul>
-          -->
-        </div>
-    </nav>
-  </header>
-  <!-- //site header -->
+
   
   <!-- banner section -->
   <section id="home" class="banner">
@@ -420,72 +381,6 @@
   </section>
   <!-- //homepage blog grids -->
   
-  <!-- site footer -->
-  <footer id="site-footer">
-    <div class="top-footer">
-      <div class="container my-md-5 my-4">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="footer-logo mb-3">
-              <a href="index.html">
-                <span class="fa fa-shield"></span> Captivate
-              </a>
-            </div>
-            <div>
-              <p class="">Lorem ipsum dolor, sit amet consectet et adipis icing elit. Ab commodi iure minus
-                laboriosam
-                placeat quia, dolorem animi. Eveniet repudiandae, perferendis nesciunt deserunt iure et, consequatur
-                optio!</p>
-            </div>
-          </div>
-          <!-- Quick Links -->
-          <div class="col-lg-3 col-md-4 mt-lg-0 mt-5">
-            <h4 class="footer-title">Quick Links</h4>
-            <ul class="footer-list">
-              <li><a href="about.html"> About company</a></li>
-              <li><a href="services.html"> Explore services</a></li>
-              <li><a href="#work"> How does we Work?</a></li>
-              <li><a href="#projects"> View projects</a></li>
-            </ul>
-          </div>
-          <!-- Newsletter -->
-          <div class="col-lg-5 col-md-8 mt-lg-0 mt-5">
-            <h4 class="footer-title">Newsletter</h4>
-            <p class="mb-4">By subscribing to our mailing list you will always be updated with the latest news from us.
-            </p>
-            <form class="newsletter-form">
-              <input class="input-rounded" type="text" required="" placeholder="Enter Email Address">
-              <button type="submit" title="Subscribe" class="btn btn-primary btn-style" name="submit" value="Submit">
-                Subscribe
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="bottom-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 text-lg-left text-center mb-lg-0 mb-3">
-            <p class="copyright">© 2020 Captivate. All Rights Reserved. Design by <a
-              href="https://w3layouts.com/">W3Layouts</a></p>
-          </div>
-          <div class="col-lg-4 align-center text-lg-right text-center">
-            <a href="#facebook" class="social-item"><span class="fa fa-facebook-f"></span></a>
-            <a href="#twitter" class="social-item"><span class="fa fa-twitter"></span></a>
-            <a href="#linkedin" class="social-item"><span class="fa fa-linkedin"></span></a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- //site footer -->
-
-  <!-- move top -->
-<button onclick="topFunction()" id="movetop" class="bg-primary" title="Go to top">
-    <span class="fa fa-angle-up"></span>
-  </button>
-
   
 </div>
 
