0d6e2b77f5e579704066c227e62dd2c8ea024e96
henry <henryonyemaobi@gmail.com>
2020-04-07 16:09:44 +0100

    fixed particles and owl carousel jquery plugins

--- static/js/custom.js
+++ static/js/custom.js
@@ -18,25 +18,7 @@
 
 
 
-$(document).ready(function () {
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-      margin: 10,
-      nav: true,
-      loop: false,
-      responsive: {
-        0: {
-          items: 1
-        },
-        767: {
-          items: 2
-        },
-        1000: {
-          items: 3
-        }
-      }
-    })
-  })
+
 
 
   $(function () {
