fdf5bd39494e32eb69aed4583b5c29660775b1b0
henry <henryonyemaobi@gmail.com>
2020-04-11 02:23:09 +0100

    added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,11 @@ export default {
     ],
 
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+     // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+     { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+     { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+     { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+     { rel: 'manifest', href: '/favicons/site.webmanifest' },
       {
         rel: 'stylesheet',
         href:
